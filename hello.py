b77e00b971e09c340f24fc7fe59d356e41089766
Badboy5555 <Badboy5555@rambler.ru>
2022-05-03 00:12:25 +0300

    asd

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ def app(environ, start_response):
     ]
     data = "\n".join(environ.get('QUERY_STRING').split("&"))
     start_response(status, response_headers)
-    return iter.([data.encode('utf-8')])
+    return iter([data.encode('utf-8')])
